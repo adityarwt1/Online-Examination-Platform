e46892408dd82fd4d997649d9d00a4f5f2375b24
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-20 14:30:48 +0530

    keywords added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import './globals.css'
 import { Navbar } from '@/components/navbar'
 
 export const metadata: Metadata = {
-  title: 'Exam Playtform',
-  description: 'Online Exam Platform',
+  title: 'Exam Platform',
+  description: 'Online Exam Platform by Aditya Rawat',
   generator: 'Aditya Rawat',
+  keywords: "online exam platform, online tests, exam preparation, competitive exams, mock tests, exam simulator, test series, question bank, practice tests, online quiz, educational platform, learning management system, assessment platform, timed tests, subject-wise tests, online study tools, exam analysis, adaptive testing, result tracking, exam portal"
 }
+
 
 export default function RootLayout({
   children,
