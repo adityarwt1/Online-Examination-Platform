d9472b47cc0415d0761fd087c6663a8dcde2cd6f
adityarwt1 <adityarawatnew2487@gmail.com>
2025-03-20 14:35:07 +0530

    mongodb fixed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = "mongodb+srv://Aditya_RWT1:14920251@adityarwt1.m01hn.mongodb.net/";
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
